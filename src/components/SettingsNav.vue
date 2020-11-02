a7583f6aef8f0173ed6fc8210ddc110aa61f0242
jensengbg-ali-suliman <alx.souliman@gmail.com>
2020-11-02 15:35:57 +0100

    Create SettingsNav.vue

---
+++ src/components/SettingsNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <h2>eezer app</h2>
+    <button class="exit">
+      <img src="@/assets/exit.svg" alt="" />
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Settings navbar",
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 1.5rem;
+
+  h2 {
+    color: #2f2f2f;
+  }
+
+  button {
+    background: #ec2329;
+    opacity: 0.6;
+    padding: 1rem;
+    border: none;
+    border-radius: 5rem;
+    transition: all 0.4s ease;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+}
+</style>
