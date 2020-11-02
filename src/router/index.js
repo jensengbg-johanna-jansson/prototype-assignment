f84f04ede15b1c3993fb131c2f3b385dc0cd2789
jensengbg-ali-suliman <alx.souliman@gmail.com>
2020-11-02 15:36:03 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,38 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import PatientStatus from '../views/PatientStatus.vue'
+import { createRouter, createWebHashHistory } from "vue-router"
+import Home from "../views/Home.vue"
+import PatientStatus from "../views/PatientStatus.vue"
+import Settings from "@/components/Settings.vue"
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: "/patient-status", 
-    name: "Pateint Status", 
-    component: PatientStatus
-  }
+    path: "/patient-status",
+    name: "Pateint Status",
+    component: PatientStatus,
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: Settings,
+  },
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
 })
 
 export default router
