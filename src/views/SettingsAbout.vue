3e65e2e08e7742f296ffbef93759daad90f2c345
jensengbg-ali-suliman <alx.souliman@gmail.com>
2020-11-03 19:41:45 +0100

    Create SettingsAbout.vue

---
+++ src/views/SettingsAbout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+    <SettingsNav />
+  </div>
+</template>
+
+<script>
+import SettingsNav from "@/components/SettingsNav.vue"
+export default {
+  components: {
+    SettingsNav,
+  },
+}
+</script>
+
+<style></style>
