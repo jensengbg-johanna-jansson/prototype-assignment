5573a1d2ed1d71615fe7dc125457cd131181467e
jensengbg-ali-suliman <alx.souliman@gmail.com>
2020-11-03 19:41:19 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
-
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 
 * {
   margin: 0rem;
   padding: 0rem;
   box-sizing: border-box;
+  outline: none;
 }
 #app {
   min-height: 100vh;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
 }
-
-
 </style>
