2551e1936cd74bdeef17472a1540cb2fb63ee566
Johanna Jansson <joja92@hotmail.com>
2020-11-04 15:03:37 +0100

    Create change password component

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import TravelHistoryExpanded from '../views/TravelHistoryExpanded.vue'
 import PatientStatus from '../views/PatientStatus.vue'
 import Pickup from '../views/Pickup.vue'
 import Dropoff from '../views/Dropoff.vue'
+import Settings from '../views/Settings.vue'
 
 const routes = [
   {
@@ -48,6 +49,11 @@ const routes = [
     path: "/patient-status",
     name: "PatientStatus",
     component: PatientStatus
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: Settings
   }
 ]
 
---
+++ src/views/Settings.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <changePassword />
+    </section>
+</template>
+
+<script>
+import changePassword from '../components/settingsComp/changePassword';
+export default {
+    name: 'Settings',
+    components: {
+        changePassword
+    }
+}
+</script>
+
+<style>
+
+</style>
