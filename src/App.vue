7e7cce0f35537dac62fda39a6fe387fd7f75e074
jensengbg-eliza-smirnova <elizasmir@gmail.com>
2020-11-05 09:38:16 +0100

    add notice

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,6 @@
 <template>
-  <transition name="fade">
-    <NewPatient v-if="openPopUP" @closeNotice="popUp" class="pop-overlay" />
-  </transition>
-  <button @click="popUp" class="btn-pop"></button>
   <router-view />
 </template>
-
-<script>
-import NewPatient from "@/views/NewPatient.vue";
-
-export default {
-  name: "App",
-  components: { NewPatient },
-  data: () => {
-    return {
-      openPopUP: false,
-    };
-  },
-  methods: {
-    popUp() {
-      console.log("wtf")
-      if (this.openPopUP === true) {
-        this.openPopUP = false;
-      } else {
-        this.openPopUP = true;
-      }
-    }
-  }
-};
-</script>
 
 <style lang="scss">
 * {
@@ -42,24 +14,4 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: $txtcolor;
 }
-
-//just for test
-.btn-pop {
-  background-color: blue;
-  cursor: pointer;
-}
-.pop-overlay {
-  height: 100vh;
-  position: absolute;
-  background-color: #000000e0;
-  z-index: 1;
-}
-// .fade-enter-active,
-// .fade-leave-active {
-//   transition: opacity 0.2s;
-// }
-// .fade-enter,
-// .fade-leave-to {
-//   opacity: 0,5;
-// }
 </style>
