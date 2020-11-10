6458a86ab6e961ca56527882ad9f949c45db5bc5
jensengbg-ali-suliman <alx.souliman@gmail.com>
2020-11-10 09:10:05 +0100

    Update SettingsAbout.vue

--- src/views/SettingsAbout.vue
+++ src/views/SettingsAbout.vue
@@ -76,17 +76,18 @@
           demonstrate, compliance with the principles.
         </p>
       </article>
+      ¨
+
       <article>
         <button @click="showTerms">terms of use</button>
         <p v-if="termsShown">
-          Last updated: (add date) Please read these Terms and Conditions
+          Last updated: 08/11/2020 Please read these Terms and Conditions
           ("Terms", "Terms and Conditions") carefully before using the
-          http://www.eezer.com (change this) website and the My Mobile App
-          (change this) mobile application (the "Service") operated by My
-          Company (change this) ("us", "we", or "our"). Your access to and use
-          of the Service is conditioned on your acceptance of and compliance
-          with these Terms. These Terms apply to all visitors, users and others
-          who access or use the Service.
+          http://www.eezer.com EEZER website and the My Mobile App EEZER mobile
+          application (the "Service") operated by My Company EEZER ("us", "we",
+          or "our"). Your access to and use of the Service is conditioned on
+          your acceptance of and compliance with these Terms. These Terms apply
+          to all visitors, users and others who access or use the Service.
         </p>
 
         <p v-if="termsShown">
@@ -123,24 +124,23 @@
         <p v-if="termsShown">
           <b>Links To Other Web Sites </b> <br />
           Our Service may contain links to third-party web sites or services
-          that are not owned or controlled by My Company (change this). My
-          Company (change this) has no control over, and assumes no
-          responsibility for, the content, privacy policies, or practices of any
-          third party web sites or services. You further acknowledge and agree
-          that My Company (change this) shall not be responsible or liable,
-          directly or indirectly, for any damage or loss caused or alleged to be
-          caused by or in connection with use of or reliance on any such
-          content, goods or services available on or through any such web sites
-          or services.
+          that are not owned or controlled by My Company EEZER. My Company EEZER
+          has no control over, and assumes no responsibility for, the content,
+          privacy policies, or practices of any third party web sites or
+          services. You further acknowledge and agree that My Company EEZER
+          shall not be responsible or liable, directly or indirectly, for any
+          damage or loss caused or alleged to be caused by or in connection with
+          use of or reliance on any such content, goods or services available on
+          or through any such web sites or services.
         </p>
 
         <p v-if="termsShown">
           <b>Changes </b> <br />
           We reserve the right, at our sole discretion, to modify or replace
           these Terms at any time. If a revision is material we will try to
-          provide at least 30 (change this) days' notice prior to any new terms
-          taking effect. What constitutes a material change will be determined
-          at our sole discretion.
+          provide at least 30 EEZER days' notice prior to any new terms taking
+          effect. What constitutes a material change will be determined at our
+          sole discretion.
         </p>
 
         <p v-if="termsShown">
